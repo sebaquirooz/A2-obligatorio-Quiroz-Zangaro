b3f2ef123d0b56524e3106ed6146e293160c5c83
sebaquirooz <sq323189@fi365.ort.edu.uy>
2026-03-25 11:56:31 -0300

    puse función hash

---
+++ funciones/hash_func.cpp
@@ -0,0 +1,10 @@
+#pragma once
+
+template <class T>
+class hash_funch{
+
+public:
+    virtual int hash(T data) = 0;
+
+
+};
---
+++ funciones/int_hash.cpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "hash_func.cpp"
+
+class int_hash: public hash_func<int>{
+public:
+    virtual int
+} 
