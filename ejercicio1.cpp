e18c86174c13ca1a24b4ac843b529d319b77ec60
sebaquirooz <sq323189@fi365.ort.edu.uy>
2026-03-26 11:57:18 -0300

    Desarrollo ideas primeros dos ejercicios

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -9,3 +9,19 @@ int main()
 {
     return 0;
 }
+
+/*
+hash cerrado, sabemos que vamos a tener bucketrs de A-Z, o sea, 27 buckets (creo)
+Definimos cont de puertas que no podemos abrir.
+
+fHash = letra - a
+ej. a - a = 0 | z - a = 27, etc.
+En el hash -> clave = letra; valor = cantidad, todas setteadas en 0, y cada vez que encontramos uno, la sumamos.
+Cuando tenemos que abrir la puerta, consultamos la posición.
+pos = fHash(letra.toLowerCase() (creo que lowerCase sería - a, o algo así))
+ si valor > 0, -- al valor y pasamos al siguiente
+ else, cont++
+
+ return contador
+
+*/
--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -10,3 +10,38 @@ int main()
     // TODO
     return 0;
 }
+
+/* 
+Idea:
+Hash cerrado, cantidad de buckets -> Cantidad de entradas.siguientePrimo()
+Definmos un ptsMaximo = minNumber y nombreMaximo = ""
+
+Si tenemos 3 entradas, a lo maximo vamos a tener 3 personas. Si vamos al siguiente primo, vamos a tener en
+el peor caso 7-3 = 4 buckets libres. Respetamos el factor de carga [0.5, 0.7], vamos a tener fC = 3/5 = 0,6.
+Es verdad que capaz usamos memoria al pedo, pero no es taaaan grave.
+
+Insertar en el hash.
+i = 0
+
+while i < M:
+    pos = (h1(clave) + i * h2(clave)) mod M
+
+    si tabla[pos] está vacía:
+        insertar (nombre, puntaje)
+        if puntaje > ptsMax
+            ptsMax = puntaje
+            nombreMaximo = clave;
+        terminar
+        terminar
+
+    si tabla[pos].nombre == clave:
+        actualizar puntaje
+        if puntaje > ptsMax
+            ptsMax = puntaje
+            nombreMaximo = clave;
+        terminar
+
+    si no:
+        i++
+
+*/
