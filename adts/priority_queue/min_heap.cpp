733d7001b12c08b11a9bd2961544df3469353eb2
sebaquirooz <sebaaquirooz@gmail.com>
2026-05-23 17:27:26 -0300

    Actualización min_heap

--- adts/priority_queue/min_heap.cpp
+++ adts/priority_queue/min_heap.cpp
@@ -68,78 +68,63 @@ private:
         }
     }
 
-  void resize(int newSize) {
-    pair *oldArr = this->arr;
-    this->arr = new pair[newSize];
-    this->arrSize = newSize;
+void resize(int newSize) {
+        pair* oldArr = this->arr;
+        this->arr = new pair[newSize];
+        this->arrSize = newSize;
 
-    for (int i = 1; i <= this->count; i++) {
-      this->arr[i] = oldArr[i];
+        for (int i = 1; i <= this->count; i++) {
+            this->arr[i] = oldArr[i];
+        }
+
+        delete[] oldArr;
     }
-
-    delete[] oldArr;
-  }
 
 public:
-  min_heap(int expectedSize) {
-    this->arr = new pair[expectedSize + 1];
-    this->count = 0;
-    this->arrSize = expectedSize + 1;
-  }
+    min_heap(int expectedSize) {
+        this->arr = new pair[expectedSize + 1];
+        this->count = 0;
+        this->arrSize = expectedSize + 1;
+    }
 
-  virtual bool isEmpty() override { return this->count == 0; }
-  virtual int size() override { return this->count; }
+    virtual bool isEmpty() override {
+        return this->count == 0;
+    }
+
+    virtual int size() override {
+        return this->count;
+    }
+
     virtual void push(E elem, P prio) override {
-    if (this->count + 1 >= this->arrSize) {
-      int newCap = this->arrSize * 2;
-      if (this->count + 2 > newCap) {
-        newCap = this->count + 2;
-      }
-      resize(newCap);
+        if (this->count + 1 >= this->arrSize) {
+            resize(this->arrSize * 2);
+        }
+
+        pair p(elem, prio);
+
+        this->count++;
+        this->arr[this->count] = p;
+
+        siftUp(this->count);
     }
 
-    this->count++;
-    this->arr[this->count] = pair(elem, prio);
-
-    siftUp(this->count);
-  }
- /* virtual void push(E elem, P prio) override {
-    if (this->count > this->arrSize) {
-      resize(this->count * 2);
+    virtual E top() override {
+        assert(!isEmpty());
+        return this->arr[1].elem;
     }
 
-    pair p = new pair(elem, prio);
-    this->count++;
-    this->arr[this->count] = p;
+    virtual E pop() override {
+        assert(!isEmpty());
 
-    siftUp(this->count);
-  } */
-  virtual E top() override {
-    assert(!isEmpty());
-    return this->arr[1].elem;
-  }
-  /*
-  virtual E pop() override {
-    assert(!isEmpty());
-    E ret = this->arr[1].elem;
+        E ret = this->arr[1].elem;
 
-    this->arr[1] = this->arr[this->count];
-    this->count--;
-    siftDown(1);
+        this->arr[1] = this->arr[this->count];
+        this->count--;
 
-    return ret;
-  }
-  */
-  virtual E pop() override {
-    assert(!isEmpty());
-    E ret = this->arr[1].elem;
+        if (this->count > 0) {
+            siftDown(1);
+        }
 
-    this->arr[1] = this->arr[this->count];
-    this->count--;
-    if (this->count > 0) {
-      siftDown(1);
+        return ret;
     }
-
-    return ret;
-  }
 };
